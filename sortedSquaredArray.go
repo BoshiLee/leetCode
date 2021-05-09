c7b9095cbc64cd8ee2e06dc98bf6cff290be667a
Boshi Li <boshi.litw@gmail.com>
2021-05-10 02:02:00 +0800

    not correct ans

--- sortedSquaredArray.go
+++ sortedSquaredArray.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(SortedSquaredArray([]int{1, 9, 49, 81, 484, 900}))
+}
+
+func SortedSquaredArray(array []int) []int {
+	left := 0
+	right := len(array) - 1
+	i := len(array) / 2
+	if len(array) % 2 == 1 {
+		array[len(array) / 2] *= array[len(array) / 2]
+	}
+	for i > 0 {
+		fmt.Println(i)
+		array[left] *= array[left]
+		array[right] *= array[right]
+		if array[left] > array[right] {
+			temp := array[right]
+			array[right] = array[left]
+			array[left] = temp
+		}
+		left ++
+		right --
+		i --
+	}
+	return array
 }
