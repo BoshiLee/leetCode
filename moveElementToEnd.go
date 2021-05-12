171621ffffd1b1228ea03a92fa0093ecbab11c5c
Boshi Li <boshi.litw@gmail.com>
2021-05-12 21:23:26 +0800

    moveElementToEnd

---
+++ moveElementToEnd.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(MoveElementToEnd([]int{2, 1, 2, 2, 2, 3, 4, 2}, 2))
+}
+
+
+func MoveElementToEnd(array []int, toMove int) []int {
+    p := 0
+    for i, val := range array {
+        if val != toMove {
+            array[i] = toMove
+            array[p] = val
+            p++
+        }
+    }
+    return array
+}
