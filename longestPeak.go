316218f1c6a5224aba915d62563955e32e6c47f0
snickers8523 <snickers8523@gmail.com>
2021-05-16 23:23:46 +0800

    optimize algorithm

--- longestPeak.go
+++ longestPeak.go
@@ -7,36 +7,32 @@ func main() {
 }
 
 func LongestPeak(array []int) int {
-	tips := map[int]int{}
-	for i, val := range array {
-		if i-1 >= 0 && i+1 < len(array) {
-			pre := array[i-1]
-			next := array[i+1]
-			if pre < val && val > next {
-				tips[val] = i
-			}
-		}
-	}
 	longPeak := 0
 	peaks := map[int]int{longPeak: 0}
-	for k, v := range tips {
-		peaks[k] = 1
-		for i := v; i > 0; i-- {
-			if array[i] > array[i-1] {
-				peaks[k] += 1
-			} else {
-				break
+	for v, val := range array {
+		if v-1 >= 0 && v+1 < len(array) {
+			pre := array[v-1]
+			next := array[v+1]
+			if pre < val && val > next {
+				peaks[val] = 1
+				for i := v; i > 0; i-- {
+					if array[i] > array[i-1] {
+						peaks[val] += 1
+					} else {
+						break
+					}
+				}
+				for i := v; i < len(array) && i + 1 < len(array); i++ {
+					if array[i] > array[i+1] {
+						peaks[val] += 1
+					} else {
+						break
+					}
+				}
+				if peaks[val] > peaks[longPeak] {
+					longPeak = val
+				}
 			}
-		}
-		for i := v; i < len(array) && i + 1 < len(array); i++ {
-			if array[i] > array[i+1] {
-				peaks[k] += 1
-			} else {
-				break
-			}
-		}
-		if peaks[k] > peaks[longPeak] {
-			longPeak = k
 		}
 	}
 	return peaks[longPeak]
